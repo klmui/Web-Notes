07e83a199b18a072f88e6ab07c3dc65f55c56c39
Kenneth Mui <klmui@wisc.edu>
2020-04-16 15:49:24 -0500

    Add home page and auth.js

--- view/src/App.js
+++ view/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import login from './pages/login';
 import signup from './pages/signup';
+import home from './pages/home';
 
 function App() {
   return (
     <Router>
       <div>
         <Switch>
-          <Route exact path="/signup" component={signup}/>
+          <Route exact path="/" component={home}/>
         </Switch>
       </div>
     </Router>
