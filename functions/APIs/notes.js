ced61e9ce682c00e8781b3a853bbf56b952e5b7c
Kenneth Mui <klmui@wisc.edu>
2020-04-16 02:08:52 -0500

    write get all notes api to fetch data

--- functions/APIs/notes.js
+++ functions/APIs/notes.js
@@ -1,15 +1,39 @@
+// exports.getAllNotes = (request, response) => {
+//   notes = [
+//     {
+//       'id': '1',
+//       'title': 'greeting',
+//       'body': 'Hello world from kenny'
+//     },
+//     {
+//       'id': '2',
+//       'title': 'greeting2',
+//       'body': 'Hello world 2 from kenny'
+//     }
+//   ]
+//   return response.json(notes);
+// }
+
+const { db } = require('../util/admin');
+
 exports.getAllNotes = (request, response) => {
-  notes = [
-    {
-      'id': '1',
-      'title': 'greeting',
-      'body': 'Hello world from kenny'
-    },
-    {
-      'id': '2',
-      'title': 'greeting2',
-      'body': 'Hello world 2 from kenny'
-    }
-  ]
-  return response.json(notes);
+  db 
+    .collection('notes')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+      let notes = [];
+      data.forEach((doc) => {
+        notes.push({
+          todoId: doc.id,
+          title: doc.data().title,
+          body: doc.data().body,
+          createdAt: doc.data().createdAt
+        });
+      });
+      return response.json(notes);
+    }).catch((err) => {
+      console.log(err);
+      return response.status(500).json({error: err.code});
+    });
 }
